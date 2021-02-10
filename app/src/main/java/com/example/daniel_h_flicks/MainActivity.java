7403e42dd6bcf52e022a2c234e21bd3ef8d396db
Daniel Hernandez <dhdan97@gmail.com>
2021-02-10 14:47:40 -0500

    Added Adapter

--- app/src/main/java/com/example/daniel_h_flicks/MainActivity.java
+++ app/src/main/java/com/example/daniel_h_flicks/MainActivity.java
@@ -1,28 +1,48 @@
 package com.example.daniel_h_flicks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.daniel_h_flicks.adapters.MovieAdapter;
+import com.example.daniel_h_flicks.models.Movie;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"; // set our api call url as a constant
-    public static final String TAG = "MainActivity" // tag used to log data when using api
-;
+    public static final String TAG = "MainActivity"; // tag used to log data when using api
+
+    List<Movie> movies; // List of movie objects from the movie class we created at Movie.java
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //Associate MainActivity.java(this file) with activity_main.xml
+        RecyclerView rvMovies = findViewById(R.id.rvMovies);
+        movies = new ArrayList<>();
+
+        // Create adapter instance
+        MovieAdapter movieAdapter = new MovieAdapter(this, movies);
+
+        // Set the adapter on the recycler view
+        rvMovies.setAdapter(movieAdapter);
+        // Set a Layout Manager
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
 
         AsyncHttpClient client = new AsyncHttpClient(); // boilerplate for GET api network request
         client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() { // notice how we use JsonHttpResponseHandler because we know the api url returns a JSON
@@ -31,8 +51,11 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onSuccess");
                 JSONObject jsonObject = json.jsonObject; // Get the json object from the api call which has the data we need
                 try {
-                    JSONArray results = jsonObject.getJSONArray("Results"); // If we looked at the json data, we would have seen that the data is stored in a array, so we do getJSONArray (make sure you handle the exception)
+                    JSONArray results = jsonObject.getJSONArray("results"); // If we looked at the json data, we would have seen that the data is stored in a array, so we do getJSONArray (make sure you handle the exception)
                     Log.i(TAG, "Results: " + results.toString());
+                    movies.addAll(Movie.fromJsonArray(results));
+                    movieAdapter.notifyDataSetChanged();
+                    Log.i(TAG,"Movies " + movies.size());
                 } catch (JSONException e) {
                     Log.e(TAG, "Hit json exception");
                 }
---
+++ app/src/main/java/com/example/daniel_h_flicks/models/Movie.java
@@ -0,0 +1,47 @@
+package com.example.daniel_h_flicks.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie { // making a class Movie that so we can put the data from the JsonObject into a java object we can use. We obtained the jsoObject back over at MainActivity.java
+
+    String posterPath;
+    String title;
+    String overview;
+    String backdropPath;
+
+    public String getPosterPath() { // get methods so we can get info from movie objects we make in MainActivity.java
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath); // Note that size(w342) is hard coded in this instance, might have to change for landscape mode
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w780/%s", backdropPath); //for landscape mode
+    }
+
+    public Movie(JSONObject jsonObject) throws JSONException { // Movies will have posters, titles, and an overview
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+        backdropPath = jsonObject.getString("backdrop_path");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException { // Making a list of Movie objects from JSON array which containsJSON objects, which are the movies
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+}
